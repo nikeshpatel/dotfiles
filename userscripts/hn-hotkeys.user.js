c55a4ff76c76c6b0aa3399feba2c50ea490f8e0e
Nikesh <np@nikeshpatel.net>
2023-01-13 11:24:05 -0600

    Fixed link selector; keycodes use code attr now

--- userscripts/hn-hotkeys.user.js
+++ userscripts/hn-hotkeys.user.js
@@ -21,16 +21,16 @@
     document.head.appendChild(ref);
     document.onkeydown = function (e) {
         //var e = e;
-        if (e.keyCode === 9 && titles_helper.length === 0) { //tab
+        if (e.code === "Tab" && titles_helper.length === 0) { //tab
             titles_helper = document.querySelectorAll('.votelinks');
             title = titles_helper[currentLink].previousSibling.previousSibling;
             comments = document.querySelectorAll('.hnuser+ .age a');
-            links = document.querySelectorAll('.votelinks+ .title .titlelink');
+            links = document.querySelectorAll('.titleline > a');
             currentLink = 0;
             title.innerText = "▶ " + title.innerText;
             e.preventDefault();
         }
-        if (e.keyCode === 39 && titles_helper!==0 && currentLink!==(titles_helper.length - 1)) { // left (to go down)
+        if (e.code === "ArrowRight" && titles_helper!==0 && currentLink!==(titles_helper.length - 1)) { // right (to go down)
             //if (currentLink === 29) {break};
             title.innerText = title.innerText.replace("▶ ","");
             currentLink = currentLink + 1;
@@ -38,20 +38,20 @@
             title.innerText = "▶ " + title.innerText;
             e.preventDefault();
         }
-        if (e.keyCode === 37 && titles_helper!==0 && currentLink!==0) { // right (to go up)
+        if (e.code === "ArrowLeft" && titles_helper!==0 && currentLink!==0) { // left (to go up)
             title.innerText = title.innerText.replace("▶ ","");
             currentLink = currentLink - 1;
             title = titles_helper[currentLink].previousSibling.previousSibling;
             title.innerText = "▶ " + title.innerText;
             e.preventDefault();
         }
-        if (e.keyCode === 13 && titles_helper!==0) { // enter
+        if (e.code === "Enter" && titles_helper!==0) { // enter
             if (document.activeElement.tagName !== "INPUT") {
                 window.open(links[currentLink].href);
                 e.preventDefault();
             }
         }
-        if (e.keyCode === 191 && titles_helper!==0) { // slash
+        if (e.code === "Slash" && titles_helper!==0) { // slash
             window.open(comments[currentLink].href);
             e.preventDefault();
         }
